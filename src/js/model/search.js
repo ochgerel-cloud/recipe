4168cb2d1c95aaf74873e888700530ee9b019214
ochgerel <ochgerel999@gmail.com>
2020-08-02 01:28:24 +0800

    first upload

---
+++ src/js/model/search.js
@@ -0,0 +1,26 @@
+require('@babel/polyfill');
+import axios from "axios";
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async doSearch() {
+
+        try {
+            const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
+
+            this.result = result.data.recipes;
+
+            return this.result;
+
+
+        } catch (error) {
+            alert("Асуудал гарлаа: " + error);
+        }
+
+
+    }
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'js/main.js',
+        path: path.resolve(__dirname, 'docs'),
+    },
+    devServer: {
+        contentBase: './docs',
+    },
+    plugins: [
+
+        new HtmlWebpackPlugin({  // Also generate a test.html
+            filename: 'index.html',
+            template: 'src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
